13f83501f4b378a7c550dd8bc5a9fa057bf4bdf3
Dawid Cieslak <cieslakdawid@gmail.com>
2018-04-23 09:14:33 +0200

    Unstage Pods

--- Pods/Target Support Files/Pods-PIDRunner/Pods-PIDRunner-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_PIDRunner : NSObject
-@end
-@implementation PodsDummy_Pods_PIDRunner
-@end
--- Pods/Target Support Files/PulseController/PulseController-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_PulseController : NSObject
-@end
-@implementation PodsDummy_PulseController
-@end
